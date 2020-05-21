376c4190255dd26fb455d3c076708883c4d4d281
javi <javitotalyuop@gmail.com>
2020-05-21 22:14:42 +0200

    init folder structure

---
+++ src/view/MoviesApp.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MoviesApp extends Component{
+
+  constructor(props :any){
+    super(props);
+    
+  }
+  render() {
+    return(
+    <div>
+Init
+    </div>);
+  }
+}
+
+export default MoviesApp;
